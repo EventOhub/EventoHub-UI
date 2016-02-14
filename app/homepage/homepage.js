910f05fd610a396972feb64cc28fafd8a7d12ef8
samarth-malhotra <malhotra.samarth@hotmail.com>
2016-02-15 02:06:22 +0530

    added the partials for main page header and footer

---
+++ app/homepage/homepage.js
@@ -0,0 +1,27 @@
+'use strict';
+angular.module('myApp.homepage', ['ngRoute'])
+
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/homepage', {
+        templateUrl: 'homepage/homepage.html',
+        controller: 'homepageCtrl'
+    });
+}])
+.controller('homepageCtrl', ['$scope',function (scope) {
+   var states = ['Rajasthan','Maharashtra','UP'];
+    scope.states = states;
+
+
+  // to make the material_select render -- overriding the default browser select
+  scope.initializeSelect = function (){
+        $('select').material_select();
+      $('.parallax').parallax();
+      $(".button-collapse").sideNav();
+
+  };
+
+// Below the initialization of materialize is done, as its overriding the default behaviour of select.
+//the below code needs to be added to the successcallback of the service    
+setTimeout(function(){ scope.initializeSelect(); }, 100);
+
+}]);
