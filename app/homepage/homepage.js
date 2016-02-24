e319bfa9e5a14721c8aa0877f4c4476ebaa01368
samarth-malhotra <malhotra.samarth@hotmail.com>
2016-02-24 23:48:40 +0530

    removing parallax

--- app/homepage/homepage.js
+++ app/homepage/homepage.js
@@ -2,26 +2,27 @@
 angular.module('myApp.homepage', ['ngRoute'])
 
 .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/homepage', {
-        templateUrl: 'homepage/homepage.html',
-        controller: 'homepageCtrl'
-    });
+        $routeProvider.when('/homepage', {
+            templateUrl: 'homepage/homepage.html',
+            controller: 'homepageCtrl'
+        });
 }])
-.controller('homepageCtrl', ['$scope',function (scope) {
-   var states = ['Rajasthan','Maharashtra','UP'];
-    scope.states = states;
+    .controller('homepageCtrl', ['$scope', function (scope) {
+        var states = ['Rajasthan', 'Maharashtra', 'UP'];
+        scope.states = states;
 
 
-  // to make the material_select render -- overriding the default browser select
-  scope.initializeSelect = function (){
-        $('select').material_select();
-      $('.parallax').parallax();
-      $(".button-collapse").sideNav();
+        // to make the material_select render -- overriding the default browser select
+        scope.initializeSelect = function () {
+            $('select').material_select();
+            $(".button-collapse").sideNav();
 
-  };
+        };
 
-// Below the initialization of materialize is done, as its overriding the default behaviour of select.
-//the below code needs to be added to the successcallback of the service    
-setTimeout(function(){ scope.initializeSelect(); }, 100);
+        // Below the initialization of materialize is done, as its overriding the default behaviour of select.
+        //the below code needs to be added to the successcallback of the service    
+        setTimeout(function () {
+            scope.initializeSelect();
+        }, 100);
 
 }]);
