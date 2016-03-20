3a56fc3fd3c085fa6e660fd5d9086f88d1d4496e
samarth-malhotra <malhotra.samarth@hotmail.com>
2016-03-20 04:44:33 -0700

    making the homepage responsive with left menu navigation and resplacing ng bind for expression tags

--- app/common/dataFactory.js
+++ app/common/dataFactory.js
@@ -1,5 +1,4 @@
 appModule.factory('dataFactory', ['$http', function($http) {
-$http.defaults.useXDomain = true;
   var urlBase = 'http://ec2-52-36-42-96.us-west-2.compute.amazonaws.com:9000';
   var dataFactory = {};
 
