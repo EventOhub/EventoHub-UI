4cf8e81f87f6996a3ade430e12dad21c109c34cf
samarth-malhotra <malhotra.samarth@hotmail.com>
2016-03-20 06:03:47 -0700

    resolved one commit issue and also committing the partial changes for vendor dashboard

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 // Declare app level module which depends on views, and components
-var appModule = angular.module('myApp', ['ngRoute','myApp.homepage','myApp.register']);
+var appModule = angular.module('myApp', ['ngRoute','myApp.homepage','myApp.register', 'myApp.dashboard']);
 
 appModule.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/homepage'});
+  //$routeProvider.otherwise({redirectTo: '/homepage'});
 }]);
--- app/header/header.js
+++ app/header/header.js
@@ -4,28 +4,30 @@ appModule.directive('header', function () {
     restrict: 'A',
     replace: true,
     templateUrl: "header/header.html",
-    controller: ['$scope', '$filter', function ($scope, $filter) {
+    controller: ['$scope', '$filter','$location', function ($scope, $filter, $location) {
       var headerOptions, brandName, loginAction, contactUsAction, joinUsAction, execute;
-      headerOptions  = [{displayName: 'Login', action: 'loginAction'}, 
-                        {displayName: 'Contact us', action: 'contactUsAction'}, 
+      headerOptions  = [{displayName: 'Login', action: 'loginAction'},
+                        {displayName: 'Contact us', action: 'contactUsAction'},
                         {displayName: 'Join us',  action: 'joinUsAction'}];
       brandName = 'EventoHub';
       $scope.headerOptions = headerOptions;
       $scope.brandName = brandName;
-        
+
       $scope.execute = function(action) {
           $scope[action]();
       };
       $scope.loginAction = function(){
-          console.log('Login');
+          //$location.path('/dashboard');
+          window.location.assign('#/dashboard');
+          console.log($location.path());
       };
       $scope.contactUsAction = function(){
           console.log('Contact Us');
       };
       $scope.joinUsAction = function(){
           $('#registrationModal').openModal();
-      }; 
-  
+      };
+
     }]
   }
 });
