8ba1a22d96ba4c80729934481690c02898ac1d00
shashijais789 <shashijais789@gmail.com>
2016-05-08 13:20:09 +0530

    Edit profile with ui router

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,12 +1,30 @@
 'use strict';
-angular.module('myApp.dashboard', ['ngRoute'])
+angular.module('myApp.dashboard', ['ngRoute', 'ui.router'])
 
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/dashboard', {
+.config(['$routeProvider', '$stateProvider', '$urlRouterProvider', function ($routeProvider, $stateProvider, $urlRouterProvider) {
+    $routeProvider.when('/dashboard', {
     templateUrl: 'dashboard/dashboard.html',
     controller: 'dashboardCtrl'
   });
+  //$routeProvider.when('/', {redirectTo: '/homepage' });    
+    
+    //$urlRouterProvider.otherwise("dashboard")
+        $stateProvider
+            .state('dashboard', {
+                url: '/dashboard',
+                templateUrl: 'dashboard/dashboard.html'
+            })
+            .state('dashboard.editProfile', {
+                //url: '/editProfile',
+            parent: 'dashboard',
+                templateUrl: 'dashboard/editProfile.html'
+            })
+            .state('dashboard.viewProfile', {
+                //url: '/viewProfile',
+            parent: 'dashboard',
+                templateUrl: 'dashboard/viewProfile.html'
+            });
 }])
-.controller('dashboardCtrl', ['$scope', 'dataFactory', function (scope, dataFactory) {
-  console.log('controller initialted..');
+    .controller('dashboardCtrl', ['$scope', 'dataFactory', function (scope, dataFactory) {
+        console.log('controller initialted..');
 }]);
